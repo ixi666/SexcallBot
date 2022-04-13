e98f8ec98019495601f89122b0d9774e5e7cd882
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-04-12 19:14:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
-global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
+global.DeveloperMode = 'falso' //verdadero o falso
+global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ', 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs', 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F', 'https: //chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['17722386341'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
-global.mods = ['5219991402134'] // No tiene utilidad
-global.prems = ['5219996125657' , '5219992095479', '972557048356'] // Podra usar algunos comandos del apartado de propietario
+global.owner = ['16042452851'] // Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.mods = ['16042452851'] // No tiene utilidad
+global.prems = ['16042452852' , '525583274312', '972557048356'] // Podra usar algunos comandos del apartado de propietario
 
 //Lo siguiente no tiene nada que ver con el funcionamiento del Bot:
-// Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalación)
+// Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalacion)
 // => Solo dudas sobre la instalación, temas generales contactarme al +17722386341 o al +5219992095479
-// => NO ayudo a hacer Bots, ni crearlos, ni editarlos 
+// => NO ayudo a hacer Bots, ni crearlos, ni editarlos
 // - Haré tutoriales de como editar el Bot, los videos los podran encontrar en mi canal de YouTube en https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA
 
-global.APIs = { // API Prefix
-  // name: 'https://website'
+global.APIs = { // Prefijo API
+  // nombre: 'https://sitio web'
   amel: 'https://melcanz.com',
   bx: 'https://bx-hunter.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -25,19 +25,19 @@ global.APIs = { // API Prefix
   zahir: 'https://zahirr-web.herokuapp.com',
   zeks: 'https://api.zeks.me',
   hardianto: 'https://hardianto.xyz',
-  pencarikode: 'https://api.chipa.xyz', 
+  pencarikode: 'https://api.chipa.xyz',
   LeysCoder: 'https://leyscoders-api.herokuapp.com',
-  adiisus: 'https://adiixyzapi.herokuapp.com',
+  adisus: 'https://adiixyzapi.herokuapp.com',
   lol: 'https://api.lolhuman.xyz',
   pencarikode: 'https://pencarikode.xyz',
-  Velgrynd: 'https://velgrynd.herokuapp.com',
-  rey: 'https://server-api-rey.herokuapp.com',
-  shadow: 'https://api.reysekha.xyz',
+  Velgurind: 'https://velgurind.herokuapp.com',
+  rey: 'https://servidor-api-rey.herokuapp.com',
+  sombra: 'https://api.reysekha.xyz',
   neoxr: 'https://api.neoxr.eu.org',
   apialc: 'https://api-alc.herokuapp.com'
 }
-global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
+global.APIKeys = { // APIKey Aquí
+  // 'https://sitio web': 'apikey'
   'https://api-alc.herokuapp.com': 'ConfuMods',
   'https://api.reysekha.xyz': 'apirey',
   'https://melcanz.com': 'F3bOrWzY',
@@ -48,29 +48,29 @@ global.APIKeys = { // APIKey Here
   'https://api.zeks.me': 'apivinz',
   'https://hardianto.xyz': 'hardianto',
   'https://api.neoxr.eu.org': 'yntkts',
-  'https://api.chipa.xyz': 'pais', 
-  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ', 
-  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.chipa.xyz': 'país',
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ',
+  'https://servidor-api-rey.herokuapp.com': 'apirey',
   'https://api.lolhuman.xyz': '9b817532fadff8fc7cb86862'
 }
 
-// Sticker WM
+// Adhesivo WM
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = 'The Shadow Brokers - Bot'
+global.author = 'The Sexcall - Bot'
 
 
-global.wm = '©The Shadow Brokers - Bot'
+global.wm = '©The Sexcall - Bot'
 global.wait = '*Espere un momento..*'
 
 
-global.multiplier = 69 // The higher, The harder levelup
+global.multiplier = 69 // Cuanto más alto, más difícil subir de nivel
 
 let fs = require('fs')
-let chalk = require('chalk')
+let tiza = require('tiza')
 let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-  fs.unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  delete require.cache[file]
-  require(file)
+fs.watchFile(archivo, () => {
+  fs.unwatchFile(archivo)
+  console.log(chalk.redBright("Actualizar 'config.js'"))
+  eliminar require.cache[archivo]
+  requerir (archivo)
 })
